b9842e924d162eb08198dda0104361cbf814a49c
namhuynh <toilanhat.nuithanh@gmail.comgit config --global user.email toilanhat.nuithanh@gmail.comgit config --global user.email toilanhat.nuithanh@gmail.com>
2025-05-15 11:15:00 +0700

    Add Ex03 and Ex04

---
+++ lab1/ex04/SinhVien.py
@@ -0,0 +1,9 @@
+class SinhVien:
+    def __init__(self, id, name, sex, major, pointAVG):
+        self._id = id
+        self._name = name
+        self._sex = sex
+        self._major =major
+        self._pointAVG = pointAVG
+        self._hocLuc = ""
+        
